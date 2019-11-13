6ff22afdcf276e172af6ab9b428d1cdf3d5f4cac
William Pang <william.pang@plymouth.ac.uk>
2019-11-13 10:26:34 +0800

    Add project files.

---
+++ GitDemoProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitDemoProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
